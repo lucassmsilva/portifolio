7469d29107a27cd8837304dc5d4025ca0f681b84
Lucas Silva <lucas.silva@piauiconectado.com.br>
2023-05-20 23:42:07 -0400

    a mobile first app with basics infomation

---
+++ app/nav.js
@@ -0,0 +1,26 @@
+// const navs = [
+//     {
+//       label: "Contact",
+//       url: "/contact",
+//       selected: true,
+//     },
+//     {
+//       label: "Projects",
+//       url: "/projects",
+//       selected: false,
+//     },
+//   ];
+
+//   {navs.map((nav) => (
+//     <div
+//       key={nav}
+//       className={
+//         "text-sm " +
+//         (nav.selected == true
+//           ? "font-bold border-b border-slate-200"
+//           : "")
+//       }
+//     >
+//       {nav.label}
+//     </div>
+//   ))}
--- app/page.js
+++ app/page.js
@@ -1,113 +1,121 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faPhone,
+  faEnvelope,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faLinkedin,
+  faWhatsapp,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
+  const Card = ({ title, children }) => {
+    return (
+      <div className="flex flex-col  mt-4 bg-slate-800 shadow-md px-2 py-1 rounded-md">
+        <div className="w-full tracking-wide mb-4 py-2 font-bold shadow-md">
+          <span className="py-2 text-xl font-semibold tracking-wide">
+            {title}
+          </span>
+        </div>
+
+        <div className="w-full ">{children}</div>
+      </div>
+    );
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col bg-slate-900 text-slate-100 lg:items-center">
+      <div className="w-full h-16 bg-gradient-to-r  from-slate-800 to-indigo-950  flex flex-row justify-between items-center px-4 py-1 tracking-wide shadow-md shadow-slate-800">
+        <div className="font-bold tracking-wide">/LUCASSMSILVA</div>
+        <div className="w-4 text-slate-200">
+          <FontAwesomeIcon icon={faBars} />
         </div>
       </div>
+      <div className="w-full flex flex-col flex-1 px-2 lg:w-2/3">
+        <Card title="CONTACT">
+          <div className="w-full flex flex-col lg:flex-row">
+            <div className="w-full flex flex-col">
+              <div className="w-[160px] h-[160px] rounded-full bg-slate-500 mb-4">
+                <Image
+                  className="rounded-full"
+                  alt="Profile Picture"
+                  src="/profile.jpg"
+                  width={160}
+                  height={160}
+                ></Image>
+              </div>
+              <div className="w-full text-lg font-semibold tracking-wider my-2">
+                FULLSTACK DEVELOPER
+              </div>
+            </div>
+            <div className="w-full flex flex-col">
+              <div className="w-full space-y-1">
+                <div className="w-full flex flex-row space-x-2">
+                  <FontAwesomeIcon className="w-4" icon={faUser} />
+                  <span>Lucas dos Santos Marques da Silva</span>
+                </div>
+                <div className="w-full flex flex-row space-x-2">
+                  <FontAwesomeIcon className="w-4" icon={faEnvelope} />
+                  <span>luca.stos@hotmail.com</span>
+                </div>
+                <div className="w-full flex flex-row space-x-2">
+                  <FontAwesomeIcon className="w-4" icon={faWhatsapp} />
+                  <span>(65) 98469-9962</span>
+                </div>
+                <div className="w-full flex flex-row space-x-2">
+                  <FontAwesomeIcon className="w-4" icon={faLinkedin} />
+                  <span>/lucassmsilva</span>
+                </div>
+              </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div className="w-full flex flex-row justify-between mt-4 mb-2 text-slate-900 lg:max-w-[300px]">
+                <a
+                  href="mailto:luca.stos@hotmail.com"
+                  target="__blank"
+                  className="bg-slate-400 px-4 py-2 rounded-lg flex items-center justify-center"
+                >
+                  <FontAwesomeIcon className="w-8" icon={faEnvelope} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/lucassmsilva/"
+                  target="__blank"
+                  className="bg-slate-400 px-4 py-2 rounded-lg flex items-center justify-center"
+                >
+                  <FontAwesomeIcon className="w-8" icon={faLinkedin} />
+                </a>
+                <a
+                  href="https://github.com/lucassmsilva"
+                  target="__blank"
+                  className="bg-slate-400 px-4 py-2 rounded-lg flex items-center justify-center"
+                >
+                  <FontAwesomeIcon className="w-8" icon={faGithub} />
+                </a>
+                <a
+                  href="https://api.whatsapp.com/send?phone=556598469962"
+                  target="__blank"
+                  className="bg-slate-400 px-4 py-2 rounded-lg flex items-center justify-center"
+                >
+                  <FontAwesomeIcon className="w-8" icon={faWhatsapp} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </Card>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <Card title="RESUME">
+          <p className="p-2">
+            Experienced developer with skills in Laravel, Vue3, and React, with
+            a passion for creating simple and efficient solutions. I am seeking
+            opportunities to grow personally and contribute to challenging
+            projects while working as part of a team. I have experience in
+            ORACLE PL/SQL queries for custom reports and filters.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </Card>
       </div>
     </main>
-  )
+  );
 }
