fb5fdb8017439fd3c11dbe8ea62a9ddbae2d0802
Lucas Silva <luca.stos@hotmail.com>
2024-01-25 00:57:34 -0400

    ajustes na web

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import {onMounted, ref} from 'vue';
+
 import { 
   BookFilled,
   CrownFilled,
@@ -10,11 +10,43 @@ import {
   YoutubeFilled,
   CodeFilled
   } from '@ant-design/icons-vue';
+
+const animmatedText = ref('');
+const textos = ref(['Fullstack Developer', 'passionate about coding']);
+const index  = ref(0);
+
+async function loopThoughText(){
+  let text = textos.value[index.value];
+  console.log(text)
+  for (let char of text){
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(animmatedText.value += char);
+        }, 150)
+      });
+  }
+
+  for (let char in text){
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(animmatedText.value = animmatedText.value.slice(0, -1));
+      }, 100)
+    });
+  }
+
+  index.value = (index.value + 1) % textos.value.length;
+  loopThoughText()
+}
+
+onMounted(() => {
+  loopThoughText();
+})
+
 </script>
 
 <template>
-  <div class="h-screen w-screen bg-gray-100 flex flex-col items-center">
-    <nav class="h-16 bg-gray-200 w-full" >
+  <div class="h-screen w-screen flex flex-col items-center dark:bg-gray-800 dark:background  dark:text-white" >
+    <nav class="h-16 bg-gray-900 w-full" >
         <ul class="w-full flex items-center justify-end gap-4 p-4 font-semibold">
           <li>
                 <a target="__blank"  class="text-xl" href="/projects" >Projects</a>
@@ -42,81 +74,85 @@ import {
         <div class="w-full flex flex-col">
           <section class="pb-4">
             <div class="w-full flex items-center justify-center pt-8 ">
-              <span class="hidden xl:block">
+              <span class="hidden xl:block hover:bg-accent dark:bg-gray-100  rounded-full">
                 <svg width="128px" height="128px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--emojione-monotone" preserveAspectRatio="xMidYMid meet"><path d="M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30s30-13.432 30-30S48.568 2 32 2zm11.275 44.508h-20.55V17.492h6.063v23.799h14.488v5.217z" fill="#000000"></path></svg>
               </span>
               <span class="xl:hidden">
                 <svg width="64px" height="64px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--emojione-monotone" preserveAspectRatio="xMidYMid meet"><path d="M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30s30-13.432 30-30S48.568 2 32 2zm11.275 44.508h-20.55V17.492h6.063v23.799h14.488v5.217z" fill="#000000"></path></svg>
               </span>
-              <span class="text-4xl xl:text-8xl font-bold text-gray-900">
+              <span class="text-4xl xl:text-8xl font-bold text-gray-100">
                 ucas Silva
               </span>
             </div>
+
           </section>
 
 
+          <div class="flex flex-row">
+            <section class="flex flex-col gap-8 pt-10">
+              <div class="border-l-2 border-accent w-full px-2">
+                <span class="text-xl">
+                  <strong>I am </strong>
+                  <em>
+                    {{ animmatedText }}
+                  </em>
+              </span>
+              </div>
+
+              <div>
+                <label class="flex gap-1 text-2xl items-baseline pb-2">
+                  <CodeFilled />
+                  <span class="font-medium font-mono">
+                    Main skills
+                  </span>
+                </label>
+
+                <ul class="font-mono flex w-full flex-wrap gap-4 pb-2">
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">C#</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">PHP</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">JAVASCRIPT</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">ORACLE PL/SQL</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">GIT</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">PYTHON</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">HTML</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">CSS</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">VUE JS</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">.NET </li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">LARAVEL</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">REACT</li>
+                  <li class="px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">REACT NATIVE</li>
+
+                </ul>
+
+              </div>
+
+              <div>
+                <label class="text-2xl font-medium font-mono">Hobbies</label>
+                <ul class="font-mono">
+                  <li>
+                    <BookFilled />
+                    Read books and light novels
+                  </li>
+                  <li>
+                    <YoutubeFilled />
+                    Learn cooking skills
+                  </li>
+                  <li>
+                    <CrownFilled />
+                    Play chess
+                  </li>
+                  <li>
+                    <SkinFilled />
+                    Go to gym
+                  </li>
+                </ul>
+              </div>
+            </section>
+
+            <img src="./assets/man_coding.svg" alt="A man coding" class="w-1/2">
+          </div>
 
 
-          <section class="flex flex-col gap-8 pt-10">
-            <div>
-              <label class="text-2xl font-medium font-mono">Who am I?</label>
-              <p class="w-full text-center font-mono">
-                A simple man motived by <strong>challenges</strong>, to create and evolve as programmer and individual
-              </p>
-            </div>
-
-            <div>
-              <label class="flex gap-1 text-2xl items-baseline">
-                <CodeFilled />
-                <span class="font-medium font-mono">
-                  Main skills
-                </span>
-              </label>
-
-              <ul class="font-mono flex w-full flex-wrap gap-4 pb-2">
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">C#</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">PHP</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">JAVASCRIPT</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">ORACLE PL/SQL</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">GIT</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">PYTHON</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">HTML</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">CSS</li>
-              </ul>
-
-              <ul class="font-mono flex gap-4">
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">VUE JS</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">.NET </li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">LARAVEL</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">REACT</li>
-                <li class="px-2 py-1 bg-gray-200 rounded-md hover:bg-gray-500 hover:font-bold hover:text-gray-100">REACT NATIVE</li>
-
-              </ul>
-              
-            </div>
-            
-            <div>
-              <label class="text-2xl font-medium font-mono">Hobbies</label>
-              <ul class="font-mono">
-                <li>
-                  <BookFilled />
-                  Read books and light novels
-                </li>
-                <li>
-                  <YoutubeFilled /> 
-                  Learn cooking skills and watch series
-                </li>
-                <li>
-                  <CrownFilled /> 
-                  Play chess
-                </li>
-                <li>
-                  <SkinFilled />
-                  Go to gym
-                </li>
-              </ul>
-            </div>
-          </section>
           </div>
         </div>
     </main>
@@ -135,5 +171,25 @@ import {
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.background {
+  background-image: url("./assets/background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+img {
+  filter: grayscale(1);
+  transition: .1s;
+}
+
+img:hover {
+  transform: rotate(-1deg) scale(1.02);
+  filter: none;
+}
+
+li:hover {
+  color: var(--accent-color);
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      gray: colors.slate,
+      accent: '#61c7c9',
+    },
     extend: {},
   },
   plugins: [],
